580184f935ad20e8d9efe844a41905f06ed3bb81
App Academy Student <email@email.com>
2022-02-21 13:51:39 -0500

    first commit

---
+++ tic_tac_toe_AI/lib/tic_tac_toe_node.rb
@@ -0,0 +1,24 @@
+require_relative 'tic_tac_toe'
+
+# write a TicTacToeNode class which utilizes the Board class from the TTT solution.
+# 
+
+
+class TicTacToeNode
+  def initialize(board, next_mover_mark, prev_move_pos = nil)
+    @board = board
+    @next_mover_mark = next_mover_mark
+    @prev_mov_pos = prev_move_pos
+  end
+
+  def losing_node?(evaluator)
+  end
+
+  def winning_node?(evaluator)
+  end
+
+  # This method generates an array of all moves that can be made after
+  # the current move.
+  def children
+  end
+end
